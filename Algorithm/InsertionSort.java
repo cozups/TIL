5d2c2660691b05f60e716c893f810e4e1e52abd4
Kim Miso <kimms9725@naver.com>
2022-03-11 23:53:16 +0900

    algorithm

---
+++ Algorithm/InsertionSort.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        int[] arr = {3,5,2,6,4,7,9,1,8};
+
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+        System.out.println();
+
+        for(int i=1; i<arr.length; i++){
+            int now = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] >= now){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = now;
+        }
+
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+    }
+}
+
