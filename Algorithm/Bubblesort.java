e9532c0c561db3f6f7e77a24070fed5a82446123
Kim Miso <kimms9725@naver.com>
2022-03-11 23:39:24 +0900

    algorithm

---
+++ Algorithm/Bubblesort.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        int[] arr = {3,5,2,6,4,7,9,1,8};
+
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+        System.out.println();
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=1; j<arr.length-i; j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+    }
+}
+
