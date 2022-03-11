d46ef43163a1566f4bf1bc7cfd1e0c3403098d38
Kim Miso <kimms9725@naver.com>
2022-03-11 23:48:11 +0900

    algorithm

---
+++ Algorithm/Selectionsort.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        int[] arr = {3,5,2,6,4,7,9,1,8};
+
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+        System.out.println();
+
+        for(int i=0; i<arr.length; i++){
+            int indexMin = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] < arr[indexMin]){
+                    indexMin = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[indexMin];
+            arr[indexMin] = temp;
+        }
+
+        for(int num: arr){
+            System.out.printf("%d, ", num);
+        }
+    }
+}
+
