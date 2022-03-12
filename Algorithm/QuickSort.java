108a5f03c57e53b8617738eb5d423ce4ba758e8f
Kim Miso <kimms9725@naver.com>
2022-03-12 10:37:34 +0900

    algorithm

--- Algorithm/QuickSort.java
+++ Algorithm/QuickSort.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
-public class Main {
+public class QuickSort {
     public static void main(String[] args) throws IOException {
         // write your code here
         int[] arr = {3,5,2,6,4,7,9,1,8};
