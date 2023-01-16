614b7cba6998094cba59ea521833bd4e6272b9a0
Kim Miso <kimms9725@naver.com>
2023-01-16 16:51:17 +0900

    SinglyLinkedList - shift

--- Algorithm/Udemy_Javascript/DataStructure/singlyLinkedList.js
+++ Algorithm/Udemy_Javascript/DataStructure/singlyLinkedList.js
@@ -37,32 +37,42 @@ class SinglyLinkedList {
   pop() {
     if (!this.head) {
       return undefined;
-    } else {
-      let temp = this.head;
-      let pre = temp;
-      while (temp.next != null) {
-        pre = temp;
-        temp = temp.next;
-      }
-
-      this.tail = pre;
-      this.tail.next = null;
-      this.length -= 1;
-
-      if (!this.length) {
-        this.head = null;
-        this.tail = null;
-      }
-
-      return temp;
     }
+    let temp = this.head;
+    let pre = temp;
+    while (temp.next != null) {
+      pre = temp;
+      temp = temp.next;
+    }
+
+    this.tail = pre;
+    this.tail.next = null;
+    this.length -= 1;
+
+    if (!this.length) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length -= 1;
+
+    if (!this.length) {
+      this.tail = null;
+    }
+
+    return currentHead;
   }
 }
 
 const list = new SinglyLinkedList();
 console.log(list.push('hi'));
 console.log(list.push('there'));
-console.log(list.pop());
-console.log(list);
-console.log(list.pop());
+console.log(list.shift());
 console.log(list);
