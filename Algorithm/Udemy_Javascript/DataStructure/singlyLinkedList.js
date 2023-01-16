19b80300e698f20e27855e9d33e1ed1c77cfbf83
Kim Miso <kimms9725@naver.com>
2023-01-16 16:46:20 +0900

    SinglyLinkedList - pop

--- Algorithm/Udemy_Javascript/DataStructure/singlyLinkedList.js
+++ Algorithm/Udemy_Javascript/DataStructure/singlyLinkedList.js
@@ -26,8 +26,43 @@ class SinglyLinkedList {
     this.length += 1;
     return this;
   }
+
+  // traverse() {
+  //   let current = this.head;
+  //   while (current != null) {
+  //     current = current.next;
+  //   }
+  // }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    } else {
+      let temp = this.head;
+      let pre = temp;
+      while (temp.next != null) {
+        pre = temp;
+        temp = temp.next;
+      }
+
+      this.tail = pre;
+      this.tail.next = null;
+      this.length -= 1;
+
+      if (!this.length) {
+        this.head = null;
+        this.tail = null;
+      }
+
+      return temp;
+    }
+  }
 }
 
 const list = new SinglyLinkedList();
 console.log(list.push('hi'));
 console.log(list.push('there'));
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);
