4529495a3f90aee0dc9b12ce680c466ec20faa9a
Kim Miso <kimms9725@naver.com>
2023-01-17 13:06:51 +0900

    DoublyLinkedList - shift & unshift

--- Algorithm/Udemy_Javascript/DataStructure/doublyLinkedList.js
+++ Algorithm/Udemy_Javascript/DataStructure/doublyLinkedList.js
@@ -47,6 +47,41 @@ class DoublyLinkedList {
 
     return poppedNode;
   }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    const oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+
+    return oldHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 list = new DoublyLinkedList();
