a8696c1e946d9739ee19934d738f813c01da46cb
Kim Miso <kimms9725@naver.com>
2023-01-17 12:50:05 +0900

    DoublyLinkedList - push & pop

--- Algorithm/Udemy_Javascript/DataStructure/doublyLinkedList.js
+++ Algorithm/Udemy_Javascript/DataStructure/doublyLinkedList.js
@@ -12,4 +12,44 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    const poppedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+
+    return poppedNode;
+  }
 }
+
+list = new DoublyLinkedList();
+list.push(1);
+list.push(99);
+console.log(list);
