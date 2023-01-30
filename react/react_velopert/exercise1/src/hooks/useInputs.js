abfe8709a9e7a3f9eda3c6b9e3e7e77e205cff84
Kim Miso <kimms9725@naver.com>
2023-01-30 14:16:43 +0900

    custom hook with useReducer

--- react/react_velopert/exercise1/src/hooks/useInputs.js
+++ react/react_velopert/exercise1/src/hooks/useInputs.js
@@ -1,17 +1,37 @@
-import { useState, useCallback } from 'react';
+import { useCallback, useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return { ...state, [action.name]: action.value };
+    case 'RESET':
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = '';
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+};
 
 const useInputs = (initialForm) => {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
 
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
 
-    setForm((form) => ({ ...form, [name]: value }));
+    dispatch({
+      type: 'CHANGE',
+      name,
+      value,
+    });
   }, []);
 
   const reset = useCallback(() => {
-    setForm(initialForm);
-  }, [initialForm]);
+    dispatch({
+      type: 'RESET',
+    });
+  }, []);
 
   return [form, onChange, reset];
 };
