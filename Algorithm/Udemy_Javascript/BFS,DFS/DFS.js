8ef6f31bf82feee36ef7376822bfd131a396d770
Kim Miso <kimms9725@naver.com>
2023-01-31 14:07:56 +0900

    DFS - tree

---
+++ Algorithm/Udemy_Javascript/BFS,DFS/DFS.js
@@ -0,0 +1,179 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let temp = this.root;
+
+      while (true) {
+        if (val === temp.value) return undefined;
+        if (val > temp.value) {
+          if (!temp.right) {
+            temp.right = newNode;
+            return this;
+          } else {
+            temp = temp.right;
+          }
+        } else if (val < temp.value) {
+          if (!temp.left) {
+            temp.left = newNode;
+            return this;
+          } else {
+            temp = temp.left;
+          }
+        }
+      }
+    }
+  }
+
+  find(val) {
+    if (!this.root) {
+      return false;
+    }
+    let temp = this.root;
+    let found = false;
+
+    while (temp && !found) {
+      if (val === temp.value) {
+        found = true;
+      } else if (val < temp.value) {
+        temp = temp.left;
+      } else {
+        temp = temp.right;
+      }
+    }
+
+    if (!found) return undefined;
+    return temp;
+  }
+
+  contains(val) {
+    if (!this.root) {
+      return false;
+    }
+    let temp = this.root;
+    let found = false;
+
+    while (temp && !found) {
+      if (val === temp.value) {
+        return true;
+      } else if (val < temp.value) {
+        temp = temp.left;
+      } else {
+        temp = temp.right;
+      }
+    }
+
+    return false;
+  }
+
+  BFS() {
+    const queue = [];
+    const visited = [];
+
+    queue.push(this.root);
+
+    while (queue.length) {
+      let current = queue.shift();
+
+      visited.push(current);
+
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+
+    return visited;
+  }
+
+  DFSPreOrder() {
+    const visited = [];
+
+    let current = this.root;
+
+    function traverse(node) {
+      visited.push(node.value);
+
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(current);
+
+    return visited;
+  }
+
+  DFSPostOrder() {
+    const visited = [];
+
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+      visited.push(node.value);
+    }
+
+    traverse(current);
+
+    return visited;
+  }
+
+  DFSInOrder() {
+    const visited = [];
+
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      visited.push(node.value);
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(current);
+
+    return visited;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
