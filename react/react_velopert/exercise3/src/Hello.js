286d842abdcbfb076286d45bfd1974d161efa658
Kim Miso <kimms9725@naver.com>
2023-02-01 23:25:51 +0900

    class component

--- react/react_velopert/exercise3/src/Hello.js
+++ react/react_velopert/exercise3/src/Hello.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
-  return (
-    <div style={{ color }}>
-      {isSpecial ? <b>*</b> : null}
-      안녕하세요 {name}
-    </div>
-  );
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  };
+
+  render() {
+    const { color, name, isSpecial } = this.props;
+
+    return (
+      <div style={{ color }}>
+        {isSpecial ? <b>*</b> : null}
+        안녕하세요 {name}{' '}
+      </div>
+    );
+  }
 }
-
-Hello.defaultProps = {
-  name: '이름없음',
-};
 
 export default Hello;
