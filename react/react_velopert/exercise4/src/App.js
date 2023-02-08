b671bb6a56f121acdbae826107497f5b75428530
Kim Miso <kimms9725@naver.com>
2023-02-08 20:56:01 +0900

    null checking

---
+++ react/react_velopert/exercise4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import User from './User';
+
+function App() {
+  const user = {
+    id: 1,
+    username: 'velopert',
+  };
+  return <User />;
+}
+
+export default App;
---
+++ react/react_velopert/exercise4/src/User.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function User({ users, onToggle }) {
+  // null checking
+  if (!users) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id} onClick={() => onToggle(user.id)}>
+          {user.username}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+User.defaultProps = {
+  onToggle: () => {
+    console.warn('onToggle is missing!');
+  },
+};
+
+export default User;
