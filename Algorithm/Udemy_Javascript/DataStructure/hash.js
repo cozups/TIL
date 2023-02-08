9cb17a65e77d8c0b0ed7f9ebc3160326f5d16564
Kim Miso <kimms9725@naver.com>
2023-02-08 13:22:19 +0900

    refined hash function

--- Algorithm/Udemy_Javascript/DataStructure/hash.js
+++ Algorithm/Udemy_Javascript/DataStructure/hash.js
@@ -1,8 +1,11 @@
 function hash(key, arrayLen) {
   let total = 0;
-  for (let char of key) {
+  // 소수를 이용하면 충돌 횟수를 줄이고 데이터가 더 퍼지게 할 수 있다.
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total + WEIRD_PRIME + value) % arrayLen;
   }
 
   return total;
