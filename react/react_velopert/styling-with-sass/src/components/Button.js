bbc2685f045ff7b4bcfa6aca5f50ead05d130692
Kim Miso <kimms9725@naver.com>
2023-02-08 21:53:44 +0900

    sass

---
+++ react/react_velopert/styling-with-sass/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Button.scss';
+import classNames from 'classnames';
+
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
+  return (
+    <button
+      className={classNames('Button', size, color, { outline, fullWidth })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
+};
+
+export default Button;
