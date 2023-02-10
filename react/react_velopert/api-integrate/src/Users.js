ee015095e85e4b72bf323ce205b0402b1c1f43e0
Kim Miso <kimms9725@naver.com>
2023-02-10 22:07:15 +0900

    API - useReducer로 관리하기

--- react/react_velopert/api-integrate/src/Users.js
+++ react/react_velopert/api-integrate/src/Users.js
@@ -1,32 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+const initialState = {
+  data: null,
+  loading: false,
+  error: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        data: null,
+        loading: true,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        data: action.data,
+        loading: false,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        data: null,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [users, setUsers] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
   const fetchUsers = async () => {
+    dispatch({
+      type: 'LOADING',
+    });
     try {
-      // initialize states
-      setError(null);
-      setUsers(null);
-      setLoading(true);
-
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users'
       );
-      setUsers(response.data);
+
+      dispatch({
+        type: 'SUCCESS',
+        data: response.data,
+      });
     } catch (e) {
-      setError(e);
+      dispatch({
+        type: 'ERROR',
+      });
     }
-    setLoading(false);
   };
 
   // useEffect의 콜백 함수에 async를 바로 붙일 수 없음 => 따로(or 안에서) async 함수 만들어서 호출
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, error, data: users } = state;
 
   if (loading) return <div>로딩 중..</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
