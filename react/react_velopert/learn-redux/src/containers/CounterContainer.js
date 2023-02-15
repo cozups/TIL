4d7ffc0459916fb9d289eafc148284d515a4f4da
Kim Miso <kimms9725@naver.com>
2023-02-16 00:29:18 +0900

    useSelector optimization

--- react/react_velopert/learn-redux/src/containers/CounterContainer.js
+++ react/react_velopert/learn-redux/src/containers/CounterContainer.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 function CounterContainer() {
   // useSelector: 리덕스 스토어의 상태를 조회하는 Hook
   // state: store.getState() 값과 동일
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
 
   // useDispatch: 리덕스 스토어의 dispatch를 사용할 수 있게 해주는 Hook
   const dispatch = useDispatch();
