4910f286a45804ca11f59067e238f49190399898
Kim Miso <kimms9725@naver.com>
2023-02-15 23:55:39 +0900

    create counter using redux

---
+++ react/react_velopert/learn-redux/src/containers/CounterContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease, setDiff } from '../modules/counter';
+
+function CounterContainer() {
+  // useSelector: 리덕스 스토어의 상태를 조회하는 Hook
+  // state: store.getState() 값과 동일
+  const { number, diff } = useSelector((state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+  }));
+
+  // useDispatch: 리덕스 스토어의 dispatch를 사용할 수 있게 해주는 Hook
+  const dispatch = useDispatch();
+  const onIncrease = () => dispatch(increase());
+  const onDecrease = () => dispatch(decrease());
+  const onSetDiff = (diff) => dispatch(setDiff(diff));
+
+  return (
+    <Counter
+      number={number}
+      diff={diff}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onSetDiff={onSetDiff}
+    />
+  );
+}
+
+export default CounterContainer;
