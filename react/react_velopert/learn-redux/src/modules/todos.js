40da3d96bd8227f32a6f66a38cc10084c002a477
Kim Miso <kimms9725@naver.com>
2023-02-16 00:25:59 +0900

    create todos using redux

--- react/react_velopert/learn-redux/src/modules/todos.js
+++ react/react_velopert/learn-redux/src/modules/todos.js
@@ -3,7 +3,7 @@ const ADD_TODO = 'todos/ADD_TODO';
 const TOGGLE_TODO = 'todos/TOGGLE_TODO';
 
 //* 액션 생성 함수
-let nextId = 1;
+let nextId = 2;
 export const addTodo = (text) => ({
   type: ADD_TODO,
   todo: {
@@ -28,9 +28,9 @@ const initialState = [
 //* 리듀서
 export default function todos(state = initialState, action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat(action.todo);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map((todo) =>
         todo.id === action.id ? { ...todo, done: !todo.done } : todo
       );
