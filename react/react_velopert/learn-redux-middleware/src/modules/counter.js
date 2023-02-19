08bf3fec5e4e397cd811b09c71de6b27121bf54e
Kim Miso <kimms9725@naver.com>
2023-02-19 17:47:02 +0900

    implement redux

---
+++ react/react_velopert/learn-redux-middleware/src/modules/counter.js
@@ -0,0 +1,22 @@
+// 액션 타입
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// 액션 생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초깃값
+const initialState = 0;
+
+// 리듀서
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
