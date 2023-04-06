d860dd736d521a83f61ea7715a936e0e0e64906a
Kim Miso <kimms9725@naver.com>
2023-04-06 17:37:04 +0900

    learned using hooks

--- typescript/react-typescript-tutorial/src/TextField.tsx
+++ typescript/react-typescript-tutorial/src/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 interface Person {
   firstName: string;
@@ -11,12 +11,21 @@ interface Props {
   i?: number;
   fn?: (bob: string) => void;
   person: Person;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const TextField: React.FC<Props> = ({ text, person }) => {
+interface TextNode {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: 'hello' });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   );
 };
